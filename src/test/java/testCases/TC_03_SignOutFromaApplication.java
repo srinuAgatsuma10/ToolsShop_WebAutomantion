942400859fc0bdcd4ab111548353d97757cfa2c9
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-05-16 13:34:59 +0530

    Generating Log Reports for FIrst Five Test Cases

--- src/test/java/testCases/TC_03_SignOutFromaApplication.java
+++ src/test/java/testCases/TC_03_SignOutFromaApplication.java
@@ -11,21 +11,28 @@ public class TC_03_SignOutFromaApplication extends BaseClass {
 
 	@BeforeClass
 	public void SignIn() {
+		logger.info("Starting TC_03_SignOutFromaApplication");
+		logger.info("Click on SigIn Button.");
 		HomePagePOM hp = new HomePagePOM(driver);
 		hp.clikcSignIn();
 
+		logger.info("Enter Valid credentials.");
 		SignInPage_POM sp = new SignInPage_POM(driver);
 		sp.enterEmailID(prop.getProperty("userName"));
 		sp.enterPasswrd(prop.getProperty("password"));
 		sp.clickLogin();
 
+		logger.info("Return to Home page.");
 		hp.clickLogo();
 	}
 
 	@Test(groups = {"Sanity","Functional","Master"})
 	public void signOutFromApplication() {
 		HomePagePOM hp = new HomePagePOM(driver);
+		logger.info("Click on MyAccount DopDown and Click on SignOut.");
 		hp.clickAccountMenu();
 		hp.clickSignOut();
+		
+		logger.info("Finishing TC_03_SignOutFromaApplication");
 	}
 }
--- src/test/java/testCases/TC_04_SearchFunctinality.java
+++ src/test/java/testCases/TC_04_SearchFunctinality.java
@@ -11,14 +11,18 @@ public class TC_04_SearchFunctinality extends BaseClass {
 
 	@Test(groups = {"Sanity","Functional","Master"})
 	public void searchProduct() {
+		logger.info("Starting TC_04_SearchFunctinality");
 		HomePagePOM hp = new HomePagePOM(driver);
+		logger.info("Search Any existing product in the search Bar and click on Search button.");
 		hp.enterProductName(prop.getProperty("productName1"));
 		hp.clearSearchButton();
 
+		logger.info("Get Names of the search results.");
 		SearchResultsPage_POM srp = new SearchResultsPage_POM(driver);
 		String proName = srp.getProductNames();
 
 		Assert.assertTrue(proName.contains(prop.getProperty("productName1")));
+		logger.info("Finishing TC_04_SearchFunctinality");
 	}
 
 }
